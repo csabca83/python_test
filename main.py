6ef105c7a749f8f43f2d146828776eebfbb2c46c
csabca83 <jagermeister1888@gmail.com>
2020-08-26 21:12:02 +0200

    Changes to be committed: 	new file:   main.py 	new file:   util/__init__.py 	new file:   util/__pycache__/__init__.cpython-38.pyc 	new file:   util/__pycache__/ht_functions.cpython-38.pyc 	new file:   util/__pycache__/list_functions.cpython-38.pyc 	new file:   util/ht_functions.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from util import ht_functions as ht
+from util import list_functions as lst
+
+test = {"hello": 4, "hey": 5}
+
+test2 = [3, 5, 6, 4324, 43243212321, 254365]
+
+print (ht.get_keys(test))
+
+print (lst.get_average(test2))
