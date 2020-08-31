fafee2791695b35a3e7352a996c69bda52bd7815
csabca83 <54415375+csabca83@users.noreply.github.com>
2020-08-31 10:35:27 +0200

    Update queryxlsx.py

--- fbchat_with_ngrok/queryexcel/queryxlsx.py
+++ fbchat_with_ngrok/queryexcel/queryxlsx.py
@@ -10,7 +10,7 @@ class Readexcel:
         self.inputWorkbook = xlrd.open_workbook(self.path)
         self.inputWorksheet = self.inputWorkbook.sheet_by_index(0)
         #print(self.inputWorksheet.nrows)
-        #print(self.inputWorksheet.ncols)
+        print(self.inputWorksheet.ncols)
         #print(self.inputWorksheet.cell_value(3, 1))
 
     def append_items(self):
@@ -19,6 +19,7 @@ class Readexcel:
         self.valasz_index = []
         self.random1 = []
         self.random2 = []
+        self.index_int = []
         for y in range(1, self.inputWorksheet.nrows):
             self.bejovo_uzenetek.append(self.inputWorksheet.cell_value(y, 1))
             self.index.append(self.inputWorksheet.cell_value(y, 2))
@@ -26,19 +27,35 @@ class Readexcel:
             self.random1.append(self.inputWorksheet.cell_value(y, 4))
             self.random2.append(self.inputWorksheet.cell_value(y, 5))
 
-        print(self.bejovo_uzenetek)
+        self.index_int = [int(i) for i in self.index]
+
+        #print(self.bejovo_uzenetek)
         #print(self.index)
+        print(self.index_int)
         #print(self.valasz_index)
         #print(self.random1)
         #print(self.random2)
 
     def append_items_2(self):
         for i, items in enumerate(self.bejovo_uzenetek):
-            if "szia" in str.lower(items):
+            if str.lower("Haliho") in str.lower(items):
                 print(items)
                 self.number_order = (i)
                 print(self.number_order)
                 break
+    
+    def find_answer(self):
+        self.answer_index = (self.index_int[self.number_order])
+        random_answers = []
+        for z in range(4, self.inputWorksheet.ncols):
+            if self.inputWorksheet.cell_value(self.answer_index, z) == "":
+                pass
+            else:
+                random_answers.append(self.inputWorksheet.cell_value(self.answer_index, z))
+
+        print(random_answers)
+        self.random_answers = random.choice(random_answers)
+        print(self.random_answers)
 
 
 r = Readexcel('answers.xlsx')
@@ -46,6 +63,8 @@ r = Readexcel('answers.xlsx')
 r.import_excel()
 r.append_items()
 r.append_items_2()
+r.find_answer()
+
 
 #path = 'answers.xlsx'
 
