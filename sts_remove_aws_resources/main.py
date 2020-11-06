0ab52821d0799f28582f440e429effd3aa96edb7
csabca83 <54415375+csabca83@users.noreply.github.com>
2020-11-06 14:53:27 +0100

    Update main.py
    updated from work pc

--- sts_remove_aws_resources/main.py
+++ sts_remove_aws_resources/main.py
@@ -2,19 +2,15 @@ from boto3_functions import Boto3_functions as b3
 from readexcel import Readexcel as rx
 from sts import sts_request as sr
 from ping import ping_request as pr
-import xlrd
+import xlrd, os, logging
+from botocore.exceptions import ClientError
 
-#TODO
-#test the ping and make sure it returns the keys
-#pr_creds = pr()
-#pr_access_key = pr_creds[0]
-#pr_secret_key = pr_creds[1]
-#pr_session_token = pr_creds[2]
-#sts_access_key = ""
-#sts_secret_key = ""
-#sts_session_token = ""
+pr_creds = pr()
+pr_access_key = pr_creds[0]
+pr_secret_key = pr_creds[1]
+pr_session_token = pr_creds[2]
 
-path = input("Paste the file name and location here, if it's in the same folder as this script the filename + extension is enough: ")#"collection_of_key_values.xlsx"
+path = "collection_of_key_values.xlsx" #input("Paste the file name and location here, if it's in the same folder as this script the filename + extension is enough: ")#"collection_of_key_values.xlsx"
 resource_column = int(input("Type the number for the column where the resource is , for example: A1 would be 0, B3 would be 1: "))
 account_column = int(input("Type the number for the column where the account ID is: "))
 name = "John"
@@ -26,22 +22,29 @@ def s3_remove():
     print("Hello")
 
 for i in range(sheet.nrows):
+
+    os.environ["AWS_ACCESS_KEY_ID"] = pr_creds[0]
+    os.environ["AWS_SECRET_ACCESS_KEY"] = pr_creds[1]
+    os.environ["AWS_SESSION_TOKEN"] = pr_creds[2]
+    os.environ["AWS_DEFAULT_REGION"] = "ca-central-1"
+
     r = rx(resource_column, account_column, i, path)
     r.reading_items()
     checked = r.check_items()
     print(checked)
+    if str(checked) == "None":
+        pass
+    else:
+        try:
 
-    #TODO
-    #RoleArn = (f"arn:aws:iam::{checked[0]}:role/<role_name>")
-    #sts_creds = sr(pr_access_key, pr_secret_key, pr_session_token, checked[0], RoleArn)
-    #sts_access_key = sts_creds[0]
-    #sts_secret_key = sts_creds[1]
-    #sts_session_token = sts_creds[2]
-    #boto_function = b3(sts_access_key, sts_secret_key, sts_session_token, checked[0], checked[1])
+            RoleArn = (f"arn:aws:iam::{str(checked[0])}:role/<put role name>")
+            sts_creds = sr(str(checked[0]), RoleArn)
 
+            boto_function = b3(checked[0], checked[1])
 
-    try:
-        exec(f"{checked[2]}()")
-        #exec(f"boto_function.{checked[2]}()")
-    except:
-        print("No function needs to be executed.")
+            exec(f"boto_function.{checked[2]}()")
+
+        except ClientError:
+            logging.error(ClientError)
+            print("No function needs to be executed.")
+            print("----------------")
