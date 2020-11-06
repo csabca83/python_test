a79104a8f119d73fe4166fe3ee88c3b11993aea5
csabca83 <54415375+csabca83@users.noreply.github.com>
2020-11-06 14:56:35 +0100

    Update boto3_functions.py
    updated from work pc

--- sts_remove_aws_resources/boto3_functions.py
+++ sts_remove_aws_resources/boto3_functions.py
@@ -1,28 +1,25 @@
 class Boto3_functions:
 
-    def __init__(self, Access_key_sts, SecretAccessKey_sts, SessionToken_sts, account_ID, resource_ID):
-        self.Access_key_sts = Access_key_sts
-        self.SecretAccessKey_sts = SecretAccessKey_sts
-        self.SessionToken_sts = SessionToken_sts
+    def __init__(self, account_ID, resource_ID):
         self.account_ID = account_ID
         self.resource_ID = resource_ID
 
     def s3_remove(self):
         import boto3
 
-        s3_client = boto3.resource('s3',
-        aws_access_key=self.Access_key_sts,
-        aws_secret_access_key=self.SecretAccessKey_sts,
-        aws_session_token=self.SessionToken_sts
-        )
+        s3_client = boto3.client('s3')
 
-        objects = s3_client.list_objects(Bucket=self.resource_ID)["Contents"]
+        try:
+            objects = s3_client.list_objects(Bucket=self.resource_ID)["Contents"]
 
-        for items in objects:
-            print(items["Key"])
-            s3_client.delete_object(Bucket=self.resource_ID, Key = items["Key"])
+            for items in objects:
+                s3_client.delete_object(Bucket=self.resource_ID, Key = items["Key"])
+                print(f'{items["Key"]} deleted.')
+        except KeyError:
+            print("No object was found.")
 
-        bucket = s3_client.Bucket(self.resource_ID)
+        bucket = boto3.resource('s3').Bucket(self.resource_ID)
 
         bucket.delete()
         print(f"{self.resource_ID} deleted.")
+        print("----------------")
