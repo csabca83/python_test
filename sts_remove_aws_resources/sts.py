a9c269f2f4cae7b9d51507d83f842c6705f8496f
csabca83 <54415375+csabca83@users.noreply.github.com>
2020-11-06 14:55:26 +0100

    Update sts.py
    updated from work pc

--- sts_remove_aws_resources/sts.py
+++ sts_remove_aws_resources/sts.py
@@ -1,14 +1,17 @@
-def sts_request(access_key, secret_key, session_token, account_ID, RoleArn):
-    import boto3
+def sts_request(account_ID, RoleArn):
+    import boto3, os
 
-    stsclient = boto3.client('sts', aws_access_key=access_key, aws_secret_access_key=secret_key, aws_session_token=session_token)
-
+    stsclient = boto3.client('sts')#, AccessKeyId=access_key, SecretAccessKey=secret_key, SessionToken=session_token)
+    
     response = stsclient.assume_role(
-        RoleArn=RoleArn
+        RoleArn=RoleArn,
+        RoleSessionName="Mysession"
     )
     
     Access_key_sts = response['Credentials']['AccessKeyId']
     SecretAccessKey_sts = response['Credentials']['SecretAccessKey']
     SessionToken_sts = response['Credentials']['SessionToken']
 
-    return Access_key_sts, SecretAccessKey_sts, SessionToken_sts
+    os.environ["AWS_ACCESS_KEY_ID"] = Access_key_sts
+    os.environ["AWS_SECRET_ACCESS_KEY"] = SecretAccessKey_sts
+    os.environ["AWS_SESSION_TOKEN"] = SessionToken_sts
